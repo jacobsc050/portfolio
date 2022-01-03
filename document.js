0b98ec10e4be27aed7b5d012e95b87d20f0d7829
Chris Jacobs <cjacobs022020@manchester.edu>
2022-01-02 21:35:04 -0500

    fix work examples

--- document.js
+++
@@ -1,7 +0,0 @@
-$(window).resize(function () { 
-    $('body').css('padding-top', parseInt($('#navbar').css("height"))+10);
-});
-
-$(window).load(function () { 
-    $('body').css('padding-top', parseInt($('#navbar').css("height"))+10);        
-});
